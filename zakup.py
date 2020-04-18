0c89c51fccdf2f56a0aba51d9ac8173e1fd709d0
Irgit <36793333+tarbagan@users.noreply.github.com>
2020-04-19 01:49:50 +0700

    Update zakup.py

--- zakup.py
+++ zakup.py
@@ -1,97 +1,123 @@
-#autor Irgit Valery
-#Многопоточный парсер госзакупок с сохранением в csv
-import requests
+"""
+Многопоточный парсер госзакупок с сохранением в csv
+https://github.com/tarbagan/zakup2
+Автор: Иргит Валерий
+Версия 2
+"""
+from multiprocessing.dummy import Pool as ThreadPool
+from dateutil.rrule import rrule, DAILY
 from bs4 import BeautifulSoup as bs
 from datetime import date
-from dateutil.rrule import rrule, DAILY
+import requests
 import re
-from multiprocessing.dummy import Pool as ThreadPool
 
-def clear(text):
-    return re.sub( r'\s+', ' ', text)
+a = date(2020, 1, 1)  # Начало периода
+b = date(2020, 4, 19)  # конец периода
+id_region = '5277386'  # id региона
+thr = 5  # кол-во потоков
+record_file = 'zakup.csv'
 
-def split(arr, count):
-    '''Делим ссылки на части для мультипотока'''
-    return [arr[i::count] for i in range(count)]
 
-def all_url():
-    '''генерируем ссылки за указанный период
-    Поменяйте в ссылке регион'''
-    a = date(2019, 1, 1) #Начало периода
-    b = date(2019, 4, 10) #конец периода
-    url_all = []
+def split(arr, thr):
+    """Делим ссылки на части для мультипотока"""
+    return [arr[i::thr] for i in range(thr)]
+
+
+def gen_url():
+    """Генерация ссылок по дате"""
+    start_url = []
     for dt in rrule(DAILY, dtstart=a, until=b):
-        date_select = (dt.strftime("%d.%m.%Y"))
-        url = 'http://www.zakupki.gov.ru/epz/order/extendedsearch/results.html?' \
-              'morphology=on&openMode=USE_DEFAULT_PARAMS&pageNumber=1&' \
-              'sortDirection=false&recordsPerPage=_100&showLotsInfoHidden=' \
-              'false&fz44=on&fz223=on&ppRf615=on&fz94=on&af=on&ca=on&pc=on&pa=' \
-              'on&currencyIdGeneral=-1&publishDateFrom={}&publishDateTo={}&region_regions_5277386=' \
-              'region_regions_5277386&regions=5277386&regionDeleted=' \
-              'false&sortBy=UPDATE_DATE'.format(date_select,date_select)
-        url_all.append(url)
-    return url_all
+        date = dt.strftime("%d.%m.%Y")
+        url = 'https://zakupki.gov.ru/epz/order/extendedsearch/results.html?searchString=&morphology=on&' \
+              'search-filter=%D0%94%D0%B0%D1%82%D0%B5+%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D1%8F&pageNumber=1&' \
+              'sortDirection=false&recordsPerPage=_100&showLotsInfoHidden=false&savedSearchSettingsIdHidden=&' \
+              'sortBy=UPDATE_DATE&fz44=on&fz223=on&ppRf615=on&fz94=on&af=on&ca=on&pc=on&pa=on&placingWayList=&' \
+              'okpd2Ids=&okpd2IdsCodes=&selectedSubjectsIdHidden=&npaHidden=&restrictionsToPurchase44=&' \
+              'publishDateFrom={}&publishDateTo={}&applSubmissionCloseDateFrom=&' \
+              'applSubmissionCloseDateTo=&priceFromGeneral=&priceFromGWS=&priceFromUnitGWS=&priceToGeneral=&' \
+              'priceToGWS=&priceToUnitGWS=&currencyIdGeneral=-1&customerIdOrg=&agencyIdOrg=&' \
+              'customerPlace=5277386&customerPlaceCodes=17000000000&OrderPlacementSmallBusinessSubject=on&' \
+              'OrderPlacementRnpData=on&OrderPlacementExecutionRequirement=on&orderPlacement94_0=0&' \
+              'orderPlacement94_1=0&orderPlacement94_2=0'.format(date, date)
+        start_url.append(url)
+    return start_url
 
-def get_page(uri):
-    '''Парсинг страницы'''
+
+def request_url(url):
+    """Получаем страницу"""
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) '
                              'AppleWebKit/537.36 (KHTML, like Gecko) '
                              'Chrome/71.0.3578.98 Safari/537.36'}
-    response = requests.get(uri, headers=headers ).text
-    soup = bs(response, 'html.parser')
-    tag = soup.findAll( "div", {"class": "registerBox"} )
-    zakup_all = []
-    for i in tag:
-        try:
-            one_columne = i.find('td')
-            text_type = one_columne.find('strong').text
-            text_fz = one_columne.find('span', {"class": "orange"}).text
-            text_rub = (one_columne.find( 'span', {"class": "fractionNumber"}).parent).text
-            text_rub = re.sub(r' ', '', clear(text_rub))
 
-            two_columne = i.find('td', {"class": "descriptTenderTd"})
-            text_num = two_columne.find('dt').text
-            text_num = (clear(text_num))
-            text_num_url = two_columne.find('dt')
-            text_num_url = 'http://www.zakupki.gov.ru' + text_num_url.find("a").get('href')
-            text_org = two_columne.find('li').text
-            text_org_url = 'http://www.zakupki.gov.ru'+(two_columne.find('li').find("a").get('href'))
-            text_main = two_columne.findAll( 'dd' )[1].text
+    try:
+        r = requests.get(url, headers=headers).text
+        soup = bs(r, 'html.parser')
+        return soup
+    except Exception as e:
+        print(e)
 
-            tree_columne = i.find('td', {"class": "amountTenderTd"})
-            text_date = (tree_columne.findAll('li')[0].text)[11:]
-            text_update = (tree_columne.findAll( 'li' )[1].text)[11:]
 
-            text_type = (clear(text_type))
-            text_org = (clear(text_org))
-            text_main = (clear(text_main))
+def clear(text):
+    text = re.sub(r'\s+', ' ', text)
+    text = re.sub(r' ₽', '', text)
+    text = text.lstrip()
+    text = text.rstrip()
+    return text
 
-            zakup = (text_type, text_fz,text_rub, text_num, text_org,
-                     text_main, text_date, text_update, text_num_url)
-            zakup_all.append(zakup)
-        except:
-            z=0
-    return zakup_all
 
-'''Парсим мультипотоком и сохраняем'''
-url_count = len(all_url())
-pagi = round(url_count / 5)
-pool = ThreadPool(5)
+def parser_start(soup):
+    arr_item = []
+    block = soup.findAll('div', {'class': 'search-registry-entry-block'})
+    try:
+        for item in block:
+            text_fz = item.find('div', {'class': 'registry-entry__header-top__title text-truncate'}).text
+            text_fz = clear(text_fz)  # Тип аукциона
+
+            text_nm = item.find('div', {'class': 'registry-entry__header-mid__number'}).text
+            text_nm = clear(text_nm)  # Код аукциона
+
+            text_ur = item.find('div', {'class': 'registry-entry__header-mid__number'}).findAll('a')[0].get(
+                'href')  # Ссылка
+
+            text_st = item.find('div', {'class': 'registry-entry__header-mid__title'}).text
+            text_st = clear(text_st)  # Статус
+
+            text_pr = item.find('div', {'class': 'price-block__value'}).text
+            text_pr = clear(text_pr)  # Статус
+
+            text_dt = item.find('div', {'class': 'data-block__value'}).text  # дата
+
+            text_ds = item.find('div', {'class': 'registry-entry__body-value'}).text
+            text_ds = clear(text_ds)  # Описание
+
+            text_zk = item.find('div', {'class': 'registry-entry__body-href'}).text
+            text_zk = clear(text_zk)  # Заказчик
+
+            data_item = (text_fz, text_nm, text_ur, text_st, text_pr, text_dt, text_ds, text_zk)
+            arr_item.append(data_item)
+    except Exception as e:
+        print(e)
+    return arr_item
+
+
+
+pool = ThreadPool(thr)
 page_count = []
+item_count = []
 
-record_file = 'zakup.csv'
 with open(record_file, 'a') as f:
-    for part in (split(all_url(), pagi)): #458
-        page_count.append(len(part))
-        print( 'Страниц обработано: {} из {}'.format(sum( page_count ), url_count))
-        try:
-            date_array = pool.map(get_page, part)
-            for sep in date_array:
-                if sep == True:
-                    for item in sep:
-                        stroka = '|'.join(item)
-                        f.write(stroka + '\n')
-        except IOError as e:
-            print("Ошибка записи в файл")
-            print(e)
-print ('Парсинг данных завершён успешно.')
+    for arr_url in split(gen_url(), thr):
+        date_array = pool.map(request_url, arr_url)
+        for page in date_array:
+            page_count.append(page)
+            data = parser_start(page)
+            if data:
+                for item in data:
+                    dat = '|'.join(item)
+                    f.write(dat + '\n')
+
+                    # логирование
+                    item_count.append(1)
+                    p_count = len(page_count)
+                    z_count = len(item_count)
+                    print(f'Страница {p_count}/закупок {z_count}: {item[1]}')
